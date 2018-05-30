99f38604152e853796a448b3186fa41320d25b01
Courtney Haltiwanger <chalt0319@gmail.com>
2018-05-30 19:01:13 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ def valid_move?(board, index)
     return true 
   elsif position_taken?(board, index) == true
     return false 
-  elsif index > 8
+  elsif board(index) == 100
     return false
   end
 end 
