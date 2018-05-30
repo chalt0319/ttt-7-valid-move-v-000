1f26936b7aaaed82dc9271493a16c073faaef9b5
Courtney Haltiwanger <chalt0319@gmail.com>
2018-05-30 19:27:07 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,7 +8,7 @@ end
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
-  elsif board[index] == "X" || board[index] == "O" || board[index] > 8
+  elsif board[index] == "X" || board[index] == "O"
     return true
   end
 end
